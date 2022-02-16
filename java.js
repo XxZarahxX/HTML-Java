41c92780e27399c416a415dd24058f081f000e11
XxZarahxX <93954064+XxZarahxX@users.noreply.github.com>
2022-02-16 14:08:07 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let appleblock = ["alma", 72432, true, [1,2]];
+
+let elso;
+let masodik = null;
+let haramadik = "NaN";
+let negyedik = null;
+
+window.alert(elso ?? masodik ?? haramadik ?? negyedik ?? "alma");
+
+console.log(ertek);
+
